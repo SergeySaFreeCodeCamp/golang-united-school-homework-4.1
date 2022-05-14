ebadff73599c3321527948600a82e4ef53db9a41
SergeySA <aledej@ya.ru>
2022-05-14 20:08:14 +0600

    lecture-04_1-solution

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,13 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
-	return output
+	r := []rune(input)
+
+	for indx1, indx2 := 0, len(r)-1; indx1 < indx2; {
+		r[indx1], r[indx2] = r[indx2], r[indx1]
+		indx1++
+		indx2--
+	}
+	output = string(r)
+	return
 }
